5f111ea0f4d4d4649ee59cf4e15f3d85424a27d1
Sebastien Ferran <sebferran@gmail.com>
2022-01-18 09:12:37 +0100

    Ajout profil utilisateur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="home")
+     * @Route("/home", name="home")
      */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'ZEUS EX MACHINA : La Mythologie Grecque à portée de clic !',
+            'controller_name' => 'Page d\'accueil',
         ]);
     }
 }
