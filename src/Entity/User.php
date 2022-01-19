58016435d0ce5c9a1d695b183dda2dab53ab576f
Sebastien Ferran <sebferran@gmail.com>
2022-01-19 12:28:12 +0100

    Mise à jour mineure

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -209,10 +209,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function __toString()
-    {
-        return $this->email;
-    }
 
     public function getNom(): ?string
     {
@@ -310,4 +306,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
     
+    public function __toString()
+    {
+        return $this->email;
+    }
 }
