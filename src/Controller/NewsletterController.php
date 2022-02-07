d942a5972cb0517bc616ea23c5c527dce9e9942f
Sebastien Ferran <sebferran@gmail.com>
2022-02-07 23:53:37 +0100

    Fonction Newsletter fonctionnelle

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,42 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
 use App\Form\NewsletterType;
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class NewsletterController extends AbstractController
 {
     /**
-     * @Route("/newsletter", name="newsletter")
+     * @Route("/newsletter", name="newsletter_new", methods={"GET", "POST"})
      */
-    public function index(Request $requete): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $formulaire = $this->createForm(NewsletterType::class);
-        $formulaire->handleRequest($requete);
-        if ($formulaire->isSubmitted())
-        {
-            $donnees = $formulaire->getData();
-            $firstname = $donnees['prenom'];
-            $lastname = $donnees['nom'];
-            $email = $donnees['mail'];
-            return $this->render('newsletter/success.html.twig',
-            [
-                'prenom' => $firstname,
-                'nom' => $lastname,
-                'mail' => $email,
-                'controller_name' => 'ZEUS EX MACHINA : Inscription réussie !',
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+
+            return $this->render('newsletter/success.html.twig', [
+                'newsletter' => $newsletter,
+                'controller_name' => 'ZEUS EX MACHINA : Inscription réussie !'
             ]);
         }
-        else
-        {
-            return $this->renderForm('newsletter/index.html.twig',
-            [
-                'formulaireAfficher' => $formulaire,
-                'controller_name' => 'ZEUS EX MACHINA : Inscription à la Newsletter',
-            ]);
-        }
+
+        return $this->renderForm('newsletter/new.html.twig', [
+            'newsletter' => $newsletter,
+            'form' => $form,
+            'controller_name' => 'ZEUS EX MACHINA : Inscription à la Newsletter'
+        ]);
     }
 }
---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsletterRepository::class)
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    // Méthode magique convertissant un tableau en chaîne de caractères à partir d'une colonne contenant une valeur en 'string'.
+    public function __toString()
+    {
+        return $this->nom;
+    }
+}
